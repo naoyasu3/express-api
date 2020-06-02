5cdeeac77c1208f1fe6f2865e3eb235466e2af83
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-06-02 17:42:52 +0900

    imgeのアップロード

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 const feedRouters = require("./routes/feed");
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 //app.use(bodyParser.urlencoded()); //x-www-form-urlencoded
 app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -21,9 +48,16 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRouters);
 
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
 mongoose
   .connect(
-    "mongodb+srv://naoyasu:kaisen29@cluster0-vleml.mongodb.net/network?retryWrites=true&w=majority",
+    "mongodb+srv://naoyasu:kaisen29@cluster0-vleml.mongodb.net/messages?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then((result) => {
