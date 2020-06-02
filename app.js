bfb61c2b5eb9a0ddf4fa45ad463bebc238875fc0
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-06-02 15:19:01 +0900

    mongoose導入

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRouters = require("./routes/feed");
 
@@ -8,6 +9,24 @@ const app = express();
 //app.use(bodyParser.urlencoded()); //x-www-form-urlencoded
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feedRouters);
 
-app.listen(8080);
+mongoose
+  .connect(
+    "mongodb+srv://naoyasu:kaisen29@cluster0-vleml.mongodb.net/network?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
